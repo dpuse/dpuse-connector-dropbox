9ff6b71b8d563efe83163c7218166521792b45fa
Jonathan Terrell <terrell.jm@gmail.com>
2026-07-07 11:31:38 +0200

    v0.2.515

--- dist/dpuse-connector-dropbox.es.js
+++ dist/dpuse-connector-dropbox.es.js
@@ -51,7 +51,7 @@ var n = {
 	vendorAccountURL: "https://www.dropbox.com/login",
 	vendorDocumentationURL: "https://help.dropbox.com/",
 	vendorHomeURL: "https://www.dropbox.com/",
-	version: "0.2.514",
+	version: "0.2.515",
 	operations: [
 		"abortOperation",
 		"auditObjectContent",
